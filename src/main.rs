bd6777586d88f7d348670719d6d8de60188081a4
VINODvoid <kalki.the.dev@gmail.com>
2026-05-25 16:14:57 +0530

    learnt: enum, functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,49 @@
-fn main() {
-    println!("Hello, world!");
-    let name = "Kalki";
-    let mut   age = 26;
-    println!("Age is {age}");
+struct User {
+        name:String,
+        age:u8,
+        email:String,
+    }
 
-     age = 25;
-    println!("My name is {name} and I am learning Rust and my age is {age}");
-    const MAX_SCORE:u32= 30;
-    println!("This is maximum score {MAX_SCORE}");
-    let mut  x  = 5;
-    x  = "hello world";
-    println!("Value of X is {x}");
-} 
+impl  User {
+    fn greet(&self){
+        println!("Hello {}", self.name)
+    }
+}
+enum Direction {
+    North,
+    South,
+    East,
+    West
+}
+enum Message{
+    Quit,
+    Move(i32,i32),
+    Write(String), 
+}
+    
+fn main(){
+    let mut user = User{
+        name:String::from("Kalki"),
+        age:26,
+        email:String::from("kalki@gmail.com")
+    };
+    println!("{} and {} and {}",user.name,user.email,user.age);
+    user.greet();
+    user.age = 65;
+    println!("{}",user.age);
+
+    let direction = Direction::North;
+    match direction {
+        Direction::North => println!("Going North"),
+        Direction::South => println!("Going South!"),
+        Direction::East => println!("Going East!"),
+        Direction::West => println!("Going West!"),
+    }
+    let msg = Message::Move(10,20);
+    match msg {
+        Message::Quit => println!("Quit"),
+      Message::Move (x, y ) => println!("Move to {} {}", x, y),
+      Message::Write(text) => println!("{}", text),
+    }
+
+}
